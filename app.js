21fa949962e703ed056d4b5b8c47d96d264c0c4a
Lorena Nogueira <lorinsantanna@gmail.com>
2026-05-18 01:06:06 -0300

    feat: configura rota para users

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require('express');
-const path = require('path');
 
-const cardsRouter = require(path.join(__dirname, 'routes', 'cards.js'));
-//const usersRouter = require(path.join(__dirname, 'routes', 'users.js'));
+const cardsRouter = require('./routes/cards');
+const usersRouter = require('./routes/users');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
-//app.use('/users', usersRouter);
+app.use('/users', usersRouter);
 app.use('/cards', cardsRouter);
 
 app.listen(PORT, () => {
---
+++ routes/users.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const path = require('path');
+const fs = require('fs').promises;
+
+const USERS_PATH = path.join(__dirname, '..', 'data', 'users.json');
+
+router.get('/', async (req, res) => {
+  try {
+    const users = JSON.parse(await fs.readFile(USERS_PATH, 'utf8'));
+    res.send(users);
+  } catch (err) {
+    console.log('Erro na leitura de dados: ', err);
+  }
+});
+
+module.exports = router;
