ce9395d0940f480b3b3649ef10a9ac5031132b92
Lorena Nogueira <lorinsantanna@gmail.com>
2026-05-18 03:14:36 -0300

    feat: adiciona mensagem de erro caso a rota não seja encontrada

--- app.js
+++ app.js
@@ -6,8 +6,13 @@ const usersRouter = require('./routes/users');
 const { PORT = 3000 } = process.env;
 const app = express();
 
+const handleRouteNotFound = (req, res) => {
+  res.status(404).send({ message: 'A solicitação não foi encontrada' });
+};
+
 app.use('/users', usersRouter);
 app.use('/cards', cardsRouter);
+app.use(handleRouteNotFound);
 
 app.listen(PORT, () => {
   console.log(`Success! App listening at port ${PORT}.`);
