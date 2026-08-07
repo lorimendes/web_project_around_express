43789867fd5e41d607e41711e261d1e35138525c
Lorena Nogueira <lorinsantanna@gmail.com>
2026-08-06 22:30:18 -0300

    feat: conecta banco de dados MongoDB via mongoose

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const cardsRouter = require('./routes/cards');
 const usersRouter = require('./routes/users');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+mongoose.connect('mongodb://localhost:27017/aroundb');
 
 const handleRouteNotFound = (req, res) => {
   res.status(404).send({ message: 'A solicitação não foi encontrada' });
