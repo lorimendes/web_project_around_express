6d7e72d4c596a10839349c41bf9c4a20ae3716c2
Lorena Nogueira <lorinsantanna@gmail.com>
2026-08-26 22:02:32 -0300

    feat: cria squema e model de card

---
+++ models/card.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true
+  },
+  link: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /^https?:\/\/(www.)?[\._~:\/\?%#\[\]@!\$&'\(\)\*\+,;=]$/.test(v);
+      },
+      message: 'Link inválido'
+    }
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true
+  },
+  likes: {
+    type: [mongoose.Schema.Types.ObjectId],
+    default: []
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const card = mongoose.model('card', cardSchema);
+
+module.exports = card;
